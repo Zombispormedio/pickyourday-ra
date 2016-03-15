d9f06ae348fc721224dec6e9ae3c437e39d05342
Xavier Serrano <zombispormedio007@gmail.com>
2016-03-15 11:12:50 +0100

    webgl

--- www/js/webgl.js
+++ www/js/webgl.js
@@ -156,6 +156,7 @@ function drawVideo(){
 function drawPicky(){
 
     mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+
     mat4.identity(mvMatrix);
     mat4.translate(mvMatrix, [0, 0.0, -7.0]);
     mat4.translate(mvMatrix, [0, 0.0, 0.0]);
